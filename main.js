cbfb73df3cd1f39e5e9a9f0aa5233fb0ae735870
mildrette <mildredfonka1@gmail.com>
2022-08-19 22:21:51 +0100

    adjusting filter

--- main.js
+++ main.js
@@ -162,26 +162,4 @@ function run(month_type) {
 
     xhttp.open("GET", "https://6047e344b801a40017ccd1df.mockapi.io/api/metrics", true);
     xhttp.send();
-}
-
-
-
-
-// onclick function for the dropdown
-
-var exampleModal = document.getElementById('Month')
-exampleModal.addEventListener('show.bs.modal', function (event) {
-  // Button that triggered the modal
-  var button = event.relatedTarget
-  // Extract info from data-bs-* attributes
-  var recipient = button.getAttribute('data-bs-whatever')
-  // If necessary, you could initiate an AJAX request here
-  // and then do the updating in a callback.
-  //
-  // Update the modal's content.
-  var modalTitle = exampleModal.querySelector('.modal-title')
-  var modalBodyInput = exampleModal.querySelector('.modal-body input')
-
-  modalTitle.textContent = 'New message to ' + recipient
-  modalBodyInput.value = recipient
-})
+};
