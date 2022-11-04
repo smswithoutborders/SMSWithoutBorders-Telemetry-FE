93ec92d574c6d6d5d4089c66b75addb4bf3db80e
Minion <nessakristo@gmail.com>
2022-11-04 09:57:46 +0100

    fixed repeating maps

--- assets/js/controller.js
+++ assets/js/controller.js
@@ -14,6 +14,38 @@ const URL = "https://62ffa49234344b6431fe43fc.mockapi.io/statistics"
 
 window.onload = () => {
 
+    document.getElementById("mapping").innerHTML = ""
+
+    anychart.onDocumentReady(function () {
+      var map = anychart.map();
+      map.geoData(anychart.maps.world);
+
+      // set the series
+      var series = map.marker();
+
+      map.title("AVAILABLE USERS AROUND THE WORLD");
+
+      anychart.theme('darkGlamour');
+
+      //format the labels of the id-defined series
+      series.labels().format("{%name}");
+
+      series.tooltip().format("Id: {%id} \nUsers: {%value}");
+
+
+      // add zoom ui controls
+      var zoomController = anychart.ui.zoom();
+      zoomController.render(map);
+
+
+      // disable labels
+      series.labels(false);
+
+      // set the container
+      map.container('mapping');
+      map.draw();
+    });
+
     let start_date_new = start_date.value
     let end_date_new = end_date.value
 
